a61481ff8976786aa82b7da323ff0078b9c143df
Jean <jeancnasc@hotmail.com>
2023-05-20 13:35:45 -0300

    versão inicial

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <img className='image-cropper' src="https://www.gravatar.com/avatar/6c6af18d483c5ee926c130a4aaa24ad9"/>
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home Page`}
+      description="Minha página pessoal">
+      <HomepageHeader />
+    </Layout>
+  );
+}
