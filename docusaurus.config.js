a2f90df1e3ed17450a3a8e0dbbb589eb93e0e863
Jean Carneiro do Nascimento <jeancnasc@hotmail.com>
2023-09-09 16:32:46 -0300

    feat: rodapé

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,19 +7,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Jean Nascimento',
-  tagline: 'Desenvolvedor Java',
-  //favicon: 'img/favicon.ico',
+  tagline: 'Desenvolvedor e Arquiteto de Software Java, entusiasta em boas práticas de codificação',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://jeancnasc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Jean Carneiro do Nascimento', // Usually your GitHub org/user name.
-  projectName: 'home-page', // Usually your repo name.
+  projectName: 'jeancnasc.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -64,11 +64,11 @@ const config = {
       // Replace with your project's social card
       //image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'jeancnasc',
-        logo: {
-          alt: 'Jean Carneiro do Nascimento',
-          src: 'https://www.gravatar.com/avatar/6c6af18d483c5ee926c130a4aaa24ad9',
-        },
+        title: 'Introdução',
+        // logo: {
+        //   alt: 'Jean Carneiro do Nascimento',
+        //   src: 'https://www.gravatar.com/avatar/6c6af18d483c5ee926c130a4aaa24ad9',
+        // },
         items: [
           // {
           //   type: 'docSidebar',
@@ -84,51 +84,49 @@ const config = {
           },
         ],
       },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Tutorial',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      // },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Autor',
+            items: [
+              {
+                html: `
+                      <div class="avatar">
+                        <a
+                          class="avatar__photo-link avatar__photo avatar__photo--lg"
+                          href="https://www.linkedin.com/in/jeancnasc/">
+                          <img
+                            alt="Jean Carneiro do Nascimento"
+                            src="https://www.gravatar.com/avatar/6c6af18d483c5ee926c130a4aaa24ad9" />
+                        </a>
+                        <div class="avatar__intro">
+                          <div class="avatar__name">Jean Carneiro do Nascimento</div>
+                          <small class="avatar__subtitle">
+                          Desenvolvedor e Arquiteto de Software Java, entusiasta em boas práticas de codificação
+                          </small>
+                        </div>
+                      </div>`
+              },
+            ],
+          },
+          
+          {
+            title: 'Redes Socias',
+            items: [
+              {
+                label: 'Linkedin',
+                href: 'https://www.linkedin.com/in/jeancnasc/'
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/jeancnasc',
+              }              
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Jean Carneiro do Nascimento.`,
+      },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
